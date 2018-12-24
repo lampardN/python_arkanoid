15dae53bd5350a9d5b0677030a34d4ec8e1dae05
LampardN <das_ist>
2018-12-25 01:40:16 +0500

    исправлен баг движения шарика + комментарии

--- test.py
+++ test.py
@@ -3,25 +3,26 @@ from brusochek import Brusochek
 from win32api import GetSystemMetrics
 import graph
 
-width = GetSystemMetrics(0) - 500 #Размер экрана
-height = GetSystemMetrics(0) - 1000 #Размер экрана
-radius = 10                       #Радиус шарика
-graph.windowSize(width + 50, height + 50)
-graph.canvasSize(width, height)
-graph.canvasPos(0, 0)
+width = GetSystemMetrics(0) - 500  # Размер экрана
+height = GetSystemMetrics(0) - 1000  # Размер экрана
+radius = 10                       # Радиус шарика
+graph.windowSize(width + 50, height + 50)  # размер окна
+graph.canvasSize(width, height)  # размер холста
+graph.canvasPos(0, 0)  # позиция холста
 
-graph.penColor('black')
-graph.penSize(5)
-graph.line(5, 5, 5, height)
-graph.line(5, 5, width, 5)
-graph.line(width, 5, width, height)
+graph.penColor('black')  # цвет рамки
+graph.penSize(5)  # ширина рамки
+graph.line(5, 5, 5, height)  # левая сторона рамки
+graph.line(5, 5, width, 5)  # вепхняя сторона рамки
+graph.line(width, 5, width, height)  # правая сторона рамки
 
-platform = Brusochek(width/3, height, 20, 15, 100).make_sqr().set_down(width)
+platform = Brusochek(width/3, height, 20, 15, 100).make_sqr().set_down(width)  # брусочек
 
 
-posX = width/2
-posY = height/2
-dot = CircleClass(posX, posY, 1, 1, radius).color(graph.randColor()).createCircle()
+posX = width/2  # позиция шарика по х
+posY = height/2  # позиция шарика по у
+dot = CircleClass(posX, posY, 1, 1, radius).color(graph.randColor()).createCircle()  # шарик
+
 
 def mov(event):
     platform.mov(width, event.keycode)
@@ -29,41 +30,34 @@ def mov(event):
 
 def update():
 
-
-
-
-    if dot.getPosition('x') + dot.radius() >= width - radius:
+    if dot.getPosition('x') + dot.radius() >= width - radius:  # если ушёл за рамку вправо
         dot.setOffset(dx=-1 * dot.getOffset('x'))
 
-    if dot.getPosition('x') - dot.radius() < -dot.radius():
+    if dot.getPosition('x') - dot.radius() < -dot.radius():  # если ушёл за рамку влево
         dot.setOffset(dx=-1 * dot.getOffset('x'))
-    if dot.getPosition('y') - dot.radius() < -dot.radius():
+    if dot.getPosition('y') - dot.radius() < -dot.radius():  # если ушёо за рамку вниз
         dot.setOffset(dy=-1 * dot.getOffset('y'))
 
-    if ((platform.position_update() + platform.w) /2)-20 <= dot.getPosition('x') + dot.radius() <= ((platform.position_update() + platform.w)/2)+20 \
-    and dot.getPosition('y') + dot.radius() == height - platform.h * 1.6:
+    if ((platform.position_update() + platform.w / 2)-20 <= dot.getPosition('x') + dot.radius() <= (platform.position_update() + platform.w/2)+20)\
+    and dot.getPosition('y') + dot.radius() == height - platform.h * 1.6:  # если шарик упал на центр платформы
         dot.setOffset(dy=-1 * dot.getOffset('y'))
         dot.setOffset(dx=0)
-        print('!!!!')
 
-    #elif (platform.position_update() + platform.w) / 2 - 20 > dot.getPosition('x') + dot.radius() >= platform.position_update()\
-    #and dot.getPosition('y') + dot.radius() == height - platform.h * 1.6:
-    #    dot.setOffset(dy=-1 * dot.getOffset('y'))
-    #    dot.setOffset(dx=1 * dot.getOffset('x'))
+    if platform.position_update() + platform.w / 2 - 20 >= dot.getPosition('x') + dot.radius() >= platform.position_update()\
+    and dot.getPosition('y') + dot.radius() == height - platform.h * 1.6:  # если шарик упал на левую половину платформы
+        dot.setOffset(dy=-1 * dot.getOffset('y'))
+        dot.setOffset(dx=1 * dot.getOffset('x'))
 
-    #elif (platform.position_update() + platform.w) / 2 + 20 < dot.getPosition('x') + dot.radius() <= platform.position_update() + platform.w\
-    #and dot.getPosition('y') + dot.radius() == height - platform.h * 1.6:
-    #    dot.setOffset(dy=-1 * dot.getOffset('y'))
-    #    dot.setOffset(dx=1 * dot.getOffset('x'))
-
-
+    if platform.position_update() + platform.w / 2 + 20 <= dot.getPosition('x') + dot.radius() <= platform.position_update() + platform.w\
+    and dot.getPosition('y') + dot.radius() == height - platform.h * 1.6:  # если упал на правую половину платформы
+        dot.setOffset(dy=-1 * dot.getOffset('y'))
+        dot.setOffset(dx=1 * dot.getOffset('x'))
 
     dot.move()
-    graph.moveObjectTo(dot.obj(), dot.getPosition('x'), dot.getPosition('y'))
+    graph.moveObjectTo(dot.obj(), dot.getPosition('x'), dot.getPosition('y'))  # движение шарика
 
-    if dot.getPosition('y') + dot.radius() >= height + radius:
+    if dot.getPosition('y') + dot.radius() >= height + radius:  # условие проигрыша
         graph.close()
-
 
 
 graph.onKey(mov)
